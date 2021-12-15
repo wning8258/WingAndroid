8c310f3c6d66d879078e05d4285026a925fb43b3
wangning <wangning@itsgmu.com>
2021-12-15 17:52:58 +0800

    feature: 去除aspectjx（与各种库不兼容，闪退，挨个exclude麻烦 也很难排查是谁的问题）

--- app/src/main/java/com/wing/android/aop/PerformanceAop.java
+++ app/src/main/java/com/wing/android/aop/PerformanceAop.java
@@ -1,33 +1,33 @@
 package com.wing.android.aop;
+//
+//import com.guagua.modules.utils.LogUtils;
+//
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.Signature;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
 
-import com.guagua.modules.utils.LogUtils;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-
-@Aspect
-public class PerformanceAop {
-
-    /**
-     * around代表在每个方法前后都插入代码
-     * 任何方法、任何参数类型都切
-     * @param joinPoint
-     */
-    @Around("call(* com.wning.demo.MyApplication.**(..))")
-    public void getTime(ProceedingJoinPoint joinPoint) {
-        LogUtils.i("PerformanceAop","getTime");
-
-        Signature signature = joinPoint.getSignature();
-
-        long startTime = System.currentTimeMillis();
-        try {
-            joinPoint.proceed();
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        }
-
-        LogUtils.i("PerformanceAop",signature.getName()+" cost "+ (System.currentTimeMillis() - startTime));
-    }
-}
+//@Aspect
+//public class PerformanceAop {
+//
+//    /**
+//     * around代表在每个方法前后都插入代码
+//     * 任何方法、任何参数类型都切
+//     * @param joinPoint
+//     */
+//    @Around("call(* com.wning.demo.MyApplication.**(..))")
+//    public void getTime(ProceedingJoinPoint joinPoint) {
+//        LogUtils.i("PerformanceAop","getTime");
+//
+//        Signature signature = joinPoint.getSignature();
+//
+//        long startTime = System.currentTimeMillis();
+//        try {
+//            joinPoint.proceed();
+//        } catch (Throwable throwable) {
+//            throwable.printStackTrace();
+//        }
+//
+//        LogUtils.i("PerformanceAop",signature.getName()+" cost "+ (System.currentTimeMillis() - startTime));
+//    }
+//}
